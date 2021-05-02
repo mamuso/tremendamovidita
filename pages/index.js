b47e3263c293c235c0f592c47255854e6894dd30
Manuel Muñoz Solera <mamuso@mamuso.net>
2021-05-01 22:45:02 -0700

    Testing getting data on render

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-export default function HomePage({ movidita }) {
+export default function HomePage() {
   const moviditas = require("../moviditas/moviditas.json");
   const movidita = moviditas.moviditas[Math.floor(Math.random() * moviditas.moviditas.length)];
   return <div>Tremenda movidita lo de {`${movidita.movidita}`} ¿Que no?!</div>;
