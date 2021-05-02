a0c0450a859b9ac2c5fc9dd7b4b717c882dd42ec
Manuel Muñoz Solera <mamuso@mamuso.net>
2021-05-01 22:44:01 -0700

    Testing getting data on render

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,5 @@
-export async function getStaticProps() {
+export default function HomePage({ movidita }) {
   const moviditas = require("../moviditas/moviditas.json");
   const movidita = moviditas.moviditas[Math.floor(Math.random() * moviditas.moviditas.length)];
-
-  return {
-    props: {
-      movidita: movidita,
-    },
-    revalidate: 1,
-  };
-}
-
-export default function HomePage({ movidita }) {
   return <div>Tremenda movidita lo de {`${movidita.movidita}`} ¿Que no?!</div>;
 }
